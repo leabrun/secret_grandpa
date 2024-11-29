4891c61f90a25d765ca37dc8011179d0d5cfadc2
gReen7aRRow <f.gusarov@inbox.ru>
2024-11-29 18:58:26 +0300

    created tg app; docker-compose was updated;

---
+++ src/bot/main.py
@@ -0,0 +1,30 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.methods import DeleteWebhook
+from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardRemove
+
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TG_TOKEN")
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands='start'))
+async def start_command(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="здарова лысый",
+                           reply_markup=ReplyKeyboardRemove())
+
+
+async def main():
+    await bot(DeleteWebhook(drop_pending_updates=True))
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
