5fa4f6dd6aac8e333cad86a9c5a1d30e3f7064e3
gReen7aRRow <f.gusarov@inbox.ru>
2024-11-29 06:42:03 +0300

    created routes: create and join team; added html pages and css; fix the main page;

--- src/web/main.py
+++ src/web/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .database.core import *
+from .database.core import get_db
+from .database.models import Teams
 
 app = FastAPI()
 
@@ -26,3 +27,31 @@ async def home_page(request: Request, db: AsyncSession = Depends(get_db)):
     return templates.TemplateResponse(
         template, context
     )
+
+
+@app.get("/create", response_class=HTMLResponse)
+async def create_page(request: Request):
+    return templates.TemplateResponse("create.html", {"request": request})
+
+
+@app.post("/create")
+async def create_team(team_title: str = Form(...),
+                      db: AsyncSession = Depends(get_db)):
+    new_team = Teams(title=team_title, owner="me", is_closed=False)
+    db.add(new_team)
+    await db.commit()
+
+    return RedirectResponse("/", status_code=303)
+
+
+@app.get("/join", response_class=HTMLResponse)
+async def join_page(request: Request):
+    return templates.TemplateResponse("join.html", {"request": request})
+
+
+@app.post("/join")
+async def join_team(team_number: str = Form(...),
+                      db: AsyncSession = Depends(get_db)):
+    # some
+
+    return RedirectResponse("/", status_code=303)
